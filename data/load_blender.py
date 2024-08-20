7012bb2905e40ed1b97c8fe8ff1ecf103e6a429c
Youssef Elkilany <youssof.kilaney@gmail.com>
2024-08-20 08:51:36 +0300

    add data loading script use load_blender script for loading compatible datasets

---
+++ data/load_blender.py
@@ -0,0 +1,105 @@
+"""
+This file is a copy of load blender from nerf-pytorch repo, I use this script since it's compatible with the data used in training.
+link: https://github.com/yenchenlin/nerf-pytorch/blob/63a5a630c9abd62b0f21c08703d0ac2ea7d4b9dd/load_blender.py
+"""
+
+import os
+import torch
+import numpy as np
+import json
+from torchvision.io import read_image
+
+
+def trans_t(t):
+    return torch.Tensor(
+        [
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, t],
+            [0, 0, 0, 1],
+        ]
+    ).float()
+
+
+def rot_phi(phi):
+    return torch.Tensor(
+        [
+            [1, 0, 0, 0],
+            [0, np.cos(phi), -np.sin(phi), 0],
+            [0, np.sin(phi), np.cos(phi), 0],
+            [0, 0, 0, 1],
+        ]
+    ).float()
+
+
+def rot_theta(th):
+    return torch.Tensor(
+        [
+            [np.cos(th), 0, -np.sin(th), 0],
+            [0, 1, 0, 0],
+            [np.sin(th), 0, np.cos(th), 0],
+            [0, 0, 0, 1],
+        ]
+    ).float()
+
+
+def pose_spherical(theta, phi, radius):
+    c2w = trans_t(radius)
+    c2w = rot_phi(phi / 180.0 * np.pi) @ c2w
+    c2w = rot_theta(theta / 180.0 * np.pi) @ c2w
+    c2w = (
+        torch.Tensor(
+            np.array([[-1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]])
+        )
+        @ c2w
+    )
+    return c2w
+
+
+def load_blender_data(basedir, testskip=1):
+    splits = ["train", "val", "test"]
+    metas = {}
+    for s in splits:
+        with open(os.path.join(basedir, "transforms_{}.json".format(s)), "r") as fp:
+            metas[s] = json.load(fp)
+
+    all_imgs = []
+    all_poses = []
+    counts = [0]
+    for s in splits:
+        meta = metas[s]
+        imgs = []
+        poses = []
+        if s == "train" or testskip == 0:
+            skip = 1
+        else:
+            skip = testskip
+
+        for frame in meta["frames"][::skip]:
+            fname = os.path.join(basedir, frame["file_path"] + ".png")
+            imgs.append(read_image(fname))
+            poses.append(torch.Tensor(frame["transform_matrix"]))
+        imgs = torch.stack(imgs).float() / 255.0
+        poses = torch.stack(poses).float()
+        counts.append(counts[-1] + imgs.shape[0])
+        all_imgs.append(imgs)
+        all_poses.append(poses)
+
+    i_split = [np.arange(counts[i], counts[i + 1]) for i in range(3)]
+
+    imgs = torch.cat(all_imgs)
+    poses = torch.cat(all_poses)
+
+    H, W = imgs[0].shape[1:]
+    camera_angle_x = float(meta["camera_angle_x"])
+    focal = 0.5 * W / np.tan(0.5 * camera_angle_x)
+
+    render_poses = torch.stack(
+        [
+            pose_spherical(angle, -30.0, 4.0)
+            for angle in np.linspace(-180, 180, 40 + 1)[:-1]
+        ],
+        0,
+    )
+
+    return imgs, poses, render_poses, [H, W, focal], i_split
